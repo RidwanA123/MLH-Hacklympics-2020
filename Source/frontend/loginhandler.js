34818860bf6723f8f2474465c45ad4443ba5723a
RidwanA123 <58993858+RidwanA123@users.noreply.github.com>
2020-12-19 17:58:28 -0500

    Log In Screen Updated

--- Source/frontend/loginhandler.js
+++ Source/frontend/loginhandler.js
@@ -1,3 +1,5 @@
+
+
 var x = 0;
 
 
@@ -5,3 +7,35 @@ var x = 0;
 function text() {
     document.getElementById("boomer").innerHTML = "Username recieved is " + document.getElementById("username").value; 
 }
+
+function showsignup() {
+    document.getElementById("newuser").type = "text"
+    document.getElementById("newpass").type = "password"
+    document.getElementById("submit").hidden = false
+    document.getElementById("signuplink").innerHTML = ""
+    document.getElementById("username").type = "hidden"
+    document.getElementById("Password").type = "hidden"
+    document.getElementById("sign in").hidden = true
+    document.getElementById("logintext").hidden = true
+    document.getElementById("signuptext").hidden = false
+    document.getElementById("signinpage").hidden = false
+}
+function backtologin() {
+    document.getElementById("newuser").type = "hidden"
+    document.getElementById("newpass").type = "hidden"
+    document.getElementById("submit").hidden = true
+    document.getElementById("signuplink").innerHTML = "Don't have an account?"
+    document.getElementById("username").type = "text"
+    document.getElementById("Password").type = "password"
+    document.getElementById("sign in").hidden = false
+    document.getElementById("logintext").hidden = false
+    document.getElementById("signuptext").hidden = true
+    document.getElementById("signinpage").hidden = true
+
+}
+
+function sendserver() {
+
+
+}
+
